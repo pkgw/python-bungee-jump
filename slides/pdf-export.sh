35be7b358b110199afad0bc674f18e69df6a3cf8
Peter Williams <peter@newton.cx>
2017-05-28 17:37:51 -0400

    slides: fix PDF export
    Decktape uses an older Webkit that requires a prefixed CSS transform attribute.

--- slides/pdf-export.sh
+++ slides/pdf-export.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
+#
+# NOTE: do *not* include print-pdf query string for this.
+
 exec docker run \
      --rm \
      --net=host \
      -v $(pwd):/slides:rw,Z \
      astefanutti/decktape \
      -s 960x720 \
-     reveal http://localhost:23232/slides.html?pdf-print export.pdf
+     reveal http://localhost:23232/slides.html export.pdf
